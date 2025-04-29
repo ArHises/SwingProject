236909ed4dd7a5a447555ca21729d7cee18d7be4
Arhises <roma1039@gmail.com>
2025-04-29 16:29:11 +0300

    Entity added

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -1,5 +1,26 @@
 package entities;
+import java.awt.*;
 
 public abstract class Entity {
-    // TODO: common features
+    protected int x, y, width, height;
+    protected int dx, dy; // תנועה
+    protected Image sprite;
+
+    public Entity(int x, int y, int width, int height, Image sprite) {
+        this.x = x; this.y = y;
+        this.width = width; this.height = height;
+        this.sprite = sprite;
+    }
+
+    public abstract void update();
+    public abstract void draw(Graphics g);
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+
+    public boolean collidesWith(Entity other) {
+        return this.getBounds().intersects(other.getBounds());
+    }
 }
+
