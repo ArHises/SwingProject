f7cdb9d97e9dbcabcf530597307ef174a723ba08
Arhises <roma1039@gmail.com>
2025-05-03 16:27:09 +0300

    GamePanel still mess... GameLoop added ;)

--- src/main/java/main/GameLoop.java
+++ src/main/java/main/GameLoop.java
@@ -1,5 +1,32 @@
 package main;
 
-public class GameLoop {
-    // TODO: make the update futures
+public class GameLoop extends Thread {
+
+    private final GamePanel GAME_PANEL;
+
+    private boolean running = true;
+    private volatile boolean paused = false;
+
+    public GameLoop(GamePanel gamePanel) {
+        this.GAME_PANEL = gamePanel;
+    }
+
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            if (!paused) {
+                GAME_PANEL.updateGame();
+                GAME_PANEL.repaint();
+            }
+
+            try {
+                Thread.sleep(16); // Roughly 60 FPS (1000 ms / 60 ≈ 16.67)
+            } catch (InterruptedException ignored) {}
+        }
+    }
+
 }
